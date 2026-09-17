9da6e6e79ce0aeea6ad5549ce00d097a5bb925b8
Ramosjhonatan-test <ramosjhonatan659@gmail.com>
2026-09-16 21:25:33 -0400

    para ti jhoss

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 onload = () => {
     document.body.classList.remove("container");
   };
+
+const musica = document.getElementById("musica-fondo");
+
+if (musica) {
+  const iniciarMusica = () => {
+    musica.play().catch(() => {});
+  };
+
+  iniciarMusica();
+  document.addEventListener("pointerdown", iniciarMusica, { once: true });
+  document.addEventListener("keydown", iniciarMusica, { once: true });
+}
