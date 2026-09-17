5cd80442182f618f39c80541edf81f14247f6f3e
Ramosjhonatan-test <ramosjhonatan659@gmail.com>
2026-09-16 21:36:25 -0400

    para ti jhoss

--- scri.js
+++ scri.js
@@ -1,19 +1,18 @@
-const poema = "Jhosselin, entre todas las flores amarillas, tú sigues siendo mi favorita: porque ninguna ilumina mis días como lo haces tú. Con cariño, Jhonatan.";
+const poema = "Llegó el 21 de septiembre,|trayendo consigo el brillo del sol;|y en cada flor amarilla|vive un destello de amor.||Tú me devuelves la calma,|Jhosselin, iluminas mi existir;|con solo ver tu sonrisa,|vuelvo, feliz, a vivir.||Eres mi eterna primavera,|mi alegría y mi ilusión;|por eso hoy, tu Jhonatan,|te entrega entero el corazón.";
 const poemaElement = document.getElementById("poema");
 const palabras = poema.split(""); // Dividir en letras
 let index = 0;
 
 function mostrarLetra() {
     if (index < palabras.length) {
-        // Si la letra es un punto, agregar un salto de línea
-        if (palabras[index] === '.') {
-            poemaElement.innerHTML += palabras[index] + "<br>"; // Agregar punto y salto de línea
+        if (palabras[index] === '|') {
+            poemaElement.innerHTML += "<br>";
         } else {
-            poemaElement.innerHTML += palabras[index]; // Agregar letra
+            poemaElement.innerHTML += palabras[index];
         }
         poemaElement.style.opacity = 1; // Hacer visible el texto
         index++;
-        setTimeout(mostrarLetra, 50); // Espera medio segundo entre letras
+        setTimeout(mostrarLetra, 28);
     }
 }
 
